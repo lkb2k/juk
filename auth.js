be9e5e7c22ce087c9a6459ab9781963a54c3c776
Lorin Kobashigawa-Bates <lorin@Lorins-MacBook-Pro.local>
2018-06-21 08:13:24 -0700

    remove nodejs scripts

--- auth.js
+++
@@ -1,8 +0,0 @@
-const SpotifyWebApi = require('spotify-web-api-node');
-const env = require('./env');
-
-var spotify = new SpotifyWebApi(env.spotify.credentials);
-
-// Create the authorization URL
-var authorizeURL = spotify.createAuthorizeURL(env.spotify.scopes);
-console.log(authorizeURL);
